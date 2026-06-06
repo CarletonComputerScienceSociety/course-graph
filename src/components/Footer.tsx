8636230f717d9a080f4bbee5d4fd3727a53a0af4
norioriroiron <97643161+norioriroiron@users.noreply.github.com>
2026-06-06 16:26:50 -0400

    implement requested changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,23 @@
 export default function Footer() {
   return (
     <footer className="bg-black border-t-red-600 border-t-2 p-1">
-      <p className="text-center text-white text-xs">
-        <b>Carleton Computer Science Society</b>
-        <br/>
-        <a href="https://github.com/CarletonComputerScienceSociety/course-graph">
-          <u>GitHub</u>
-        </a>
-        <br/>
-        Not affiliated with or endorsed by Carleton University.
-        <br/>
-        Always verify course requirements in the official undergraduate calendar.
-        <br/>
-        © {new Date().getFullYear()} Carleton Computer Science Society
-      </p>
+      <div className="text-center text-white text-xs">
+        <p className="text-lg font-semibold">
+          <a href="https://ccss.carleton.ca/">
+            Carleton Computer Science Society
+          </a>
+        </p>
+        <p>
+          Not affiliated with or endorsed by Carleton University.{' '}
+          Always verify course requirements in the{' '}
+          <a className="underline" href="https://calendar.carleton.ca/undergrad/undergradprograms/computerscience/">
+            official undergraduate calendar
+          </a>.
+        </p>
+        <p className="text-sm">
+          © {new Date().getFullYear()} Carleton Computer Science Society - <a className="underline" href="https://github.com/CarletonComputerScienceSociety/course-graph">GitHub</a>
+        </p>
+      </div>
     </footer>
   );
 }
